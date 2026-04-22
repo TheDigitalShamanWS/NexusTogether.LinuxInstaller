5cf43001d2504ae3b0d79bb275290aca52c0ffe3
TheDigitalShamanWS <username@users.noreply.github.com>
2026-04-22 20:08:26 +0100

    Fixed .net variables in service wrappers

--- wrappers/wrapper_service.sh
+++ wrappers/wrapper_service.sh
@@ -11,6 +11,13 @@ if [[ -z "$SERVICE_NAME" || -z "$SERVICE_PATH" || -z "$SERVICE_PROJECT_NAME" ]];
     exit 1
 fi
 
+# Set .NET environment variables
+export PATH="$HOME/.dotnet:$HOME/.dotnet/tools:$PATH"
+export DOTNET_ROOT="$HOME/.dotnet"
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+
 # Simple log file in services directory
 LOG_FILE="$SERVICES_DIR/nexus-${SERVICE_NAME}.log"
 
